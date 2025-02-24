a5954b827898b298300327d94f77bbded5845c94
Benedetta9708 <74251809+benedetta9708@users.noreply.github.com>
2025-02-24 10:44:33 +0100

    Update menu.pl

--- menu.pl
+++ menu.pl
@@ -12,14 +12,18 @@ hypothesize(carbonara) :- carbonara, !.
 hypothesize(risotto_ai_funghi) :- risotto_ai_funghi, !.
 hypothesize(bistecca_alla_fiorentina) :- bistecca_alla_fiorentina, !.
 hypothesize(minestrone) :- minestrone, !.
+hypothesize(insalata_caprese) :- insalata_caprese, !.
 hypothesize(not_in_my_memory). % Nessun pasto corrispondente
 
 % Regole per identificare i pasti
-pizza_margherita :- italiano, vegetariano, verify(has_pomodoro), verify(has_mozzarella).
+pizza_margherita :- italiano, vegetariano, verify(has_pomodoro), verify(has_mozzarella),not_lactose_intolerant.
 carbonara :- italiano, carnivoro, verify(has_uova), verify(has_pancetta), verify(has_pasta).
 risotto_ai_funghi :- italiano, vegetariano, verify(has_riso), verify(has_funghi).
 bistecca_alla_fiorentina :- italiano, carnivoro, verify(has_manzo), verify(grigliato).
 minestrone :- italiano, vegano, verify(has_verdure), verify(has_brodo).
+insalata_caprese :- italian, has_dairy, 
+                     verify(has_tomato), verify(has_cheese), 
+                     not_lactose_intolerant.
 
 % Classificazione dei pasti
 italiano :- verify(cucina_italiana).
@@ -29,6 +33,7 @@ carnivoro :- verify(has_carne).
 senza_glutine :- \+ verify(has_pasta), \+ verify(has_pane).
 ipo_calorico :- verify(calorie_basse).
 alta_caloria :- verify(calorie_alte).
+not_lactose_intolerant :- (verify(intolleranza_lattosio) -> fail ; true).
 
 % Metodo per fare domande
 ask(Question) :- 
