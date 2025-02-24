55141889013ead30a4d76cc9bc9d71456aa87ae9
Benedetta9708 <74251809+benedetta9708@users.noreply.github.com>
2025-02-24 10:25:30 +0100

    Update menu.pl

--- menu.pl
+++ menu.pl
@@ -1,1 +1,50 @@
-prolog
+% -*- mode: Prolog -*-
+% Sistema di selezione dei pasti per un ristorante italiano
+
+% Avvio del sistema
+go :- hypothesize(Pasto), 
+      write('Il pasto suggerito è: '), 
+      write(Pasto), nl, undo.
+
+% Ipotesi di pasti
+hypothesize(pizza_margherita) :- pizza_margherita, !.
+hypothesize(carbonara) :- carbonara, !.
+hypothesize(risotto_ai_funghi) :- risotto_ai_funghi, !.
+hypothesize(bistecca_alla_fiorentina) :- bistecca_alla_fiorentina, !.
+hypothesize(minestrone) :- minestrone, !.
+hypothesize(not_in_my_memory). % Nessun pasto corrispondente
+
+% Regole per identificare i pasti
+pizza_margherita :- italiano, vegetariano, verify(has_pomodoro), verify(has_mozzarella).
+carbonara :- italiano, carnivoro, verify(has_uova), verify(has_pancetta), verify(has_pasta).
+risotto_ai_funghi :- italiano, vegetariano, verify(has_riso), verify(has_funghi).
+bistecca_alla_fiorentina :- italiano, carnivoro, verify(has_manzo), verify(grigliato).
+minestrone :- italiano, vegano, verify(has_verdure), verify(has_brodo).
+
+% Classificazione dei pasti
+italiano :- verify(cucina_italiana).
+vegetariano :- \+ verify(has_carne), \+ verify(has_pesce).
+vegano :- vegetariano, \+ verify(has_formaggio), \+ verify(has_uova).
+carnivoro :- verify(has_carne).
+senza_glutine :- \+ verify(has_pasta), \+ verify(has_pane).
+ipo_calorico :- verify(calorie_basse).
+alta_caloria :- verify(calorie_alte).
+
+% Metodo per fare domande
+ask(Question) :- 
+        write('Il pasto ha il seguente attributo: '), 
+        write(Question), write('? '), 
+        read(Response), nl, 
+        ( (Response == yes ; Response == y) 
+        -> assert(yes(Question)) ; 
+        assert(no(Question)), fail). 
+
+% Verifica delle risposte
+:- dynamic yes/1,no/1.
+verify(S) :- (yes(S) -> true ; (no(S) -> fail ; ask(S))). 
+
+% Resetta le risposte
+undo :- retract(yes(_)),fail. 
+undo :- retract(no(_)),fail. 
+undo.  
+
